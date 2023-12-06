166b59deb10d63584e0c7db287343a355df6b3c5
alton-gunawan <65076612+Ivanyudinata@users.noreply.github.com>
2023-12-06 17:04:37 +0800

    Update ML Model

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify
-from flask import render_template
+from pyspark.ml.recommendation import ALSModel
+from pyspark.sql import SparkSession
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
-def hello_world():
-    return jsonify({ 'msg': "Hello world" })
+def getRecommendationBooks():
+    # Create a Spark session
+    spark = SparkSession.builder.appName("Librari").getOrCreate()
+
+    # Load the ALS model
+    model = ALSModel.load("librari_model")
+    recommended_user = model.recommendForAllUsers(8)
+
+    filtered = recommended_user.select("recommendations")
+    return recommended_user.collect()
